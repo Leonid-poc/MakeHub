e0d4c7e0007b93675108a91484c362714bc92ce2
Leonid <legeorg2004@gmail.com>
2022-12-29 21:37:41 +0500

    добавление БД инструкций а также шифратор паролей

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Test
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizza.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
 
 @app.route('/')
 def index():
@@ -10,4 +17,3 @@ def index():
 
 if __name__ == '__main__':
     app.run('localhost', 5000, debug=True)
-# Тест для Ранила с большой буквы
