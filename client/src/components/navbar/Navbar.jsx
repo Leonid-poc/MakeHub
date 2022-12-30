4b8a96718587d50e62650c503beda163b729cb3c
Russian-Idiot <ranil2086@bk.ru>
2022-12-30 17:31:43 +0500

    reboot

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -8,14 +8,13 @@ import Navigation from './Navigation';
 const Navbar = () => {
   return (
     <nav className='main_nav'>
-        <div className="icon">
-            <h1 style={{color: "white"}}>ЧЛЕН</h1>
-        </div>
-        <div className={classes.NavBar}>
-          <MobileNavigation />
-          <Navigation />
-        </div>
-        {/* <div className="menu2" style={{display: "none"}}>MENU</div> */}
+      <div className="icon">
+          <h1 style={{color: "white"}}>ЧЛЕН</h1>
+      </div>
+      <div className={classes.NavBar}>
+        <MobileNavigation />
+        <Navigation />
+      </div>
     </nav>
   )
 }
