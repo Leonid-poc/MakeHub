29338ea880cec46d9ae0d5e50588002516a79a33
Russian-Idiot <ranil2086@bk.ru>
2022-12-30 19:04:06 +0500

    welcome page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Info from './components/Info';
 import Main from './components/Main';
 import Navbar from './components/navbar/Navbar';
+import Restouran from './components/Restouran';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />    
       <Main />
       <Info />
+      <Restouran />
     </>
   )
 }
