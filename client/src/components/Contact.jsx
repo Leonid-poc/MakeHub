dcf90d3a01e08d986006439a8bf2b2dde56292bf
Russian-Idiot <ranil2086@bk.ru>
2023-01-04 18:41:06 +0500

    last upgrade

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/Contact.css';
 import SimpleMap from './items/map';
 import Fade from 'react-reveal/Fade';
 
 const Contact = () => {
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const [input3, setInput3] = useState('');
+  const [input4, setInput4] = useState('');
+
   return (
     <section className='contact'>
       <div className="map_section">
@@ -16,10 +21,10 @@ const Contact = () => {
           </div>    
         </Fade>
         <div className="contact_form">
-          <Fade right><input type="text" placeholder='First Name'/></Fade>
-          <Fade right><input type="text" placeholder='Second Name'/></Fade>
-          <Fade right><input type="tel" placeholder='Phone Number'/></Fade>
-          <Fade right><input type="text" placeholder='Message'/></Fade>
+          <Fade right><input type="text" placeholder='First Name' onChange={e => setInput1(e.target.value)} style={input1 != "" ? {borderBottom: '1px solid #fac564'} : null}/></Fade>
+          <Fade right><input type="text" placeholder='Second Name' onChange={e => setInput2(e.target.value)} style={input2 != "" ? {borderBottom: '1px solid #fac564'} : null}/></Fade>
+          <Fade right><input type="tel" placeholder='Phone Number' onChange={e => setInput3(e.target.value)} style={input3 != "" ? {borderBottom: '1px solid #fac564'} : null} /></Fade>
+          <Fade right><input type="text" placeholder='Message' onChange={e => setInput4(e.target.value)} style={input4 != "" ? {borderBottom: '1px solid #fac564'} : null}/></Fade>
         </div> 
         <Fade bottom>
           <div className='contact_button'>
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,70 +5,70 @@ import { SlSocialInstagram, SlSocialFacebook, SlSocialTwitter } from 'react-icon
 import {MdEmail, MdLocationPin} from 'react-icons/md';
 
 const Footer = () => {
-  return (
-    <footer className='footer'>
-        <div className="footer_container">
-            <div className="footer_container_top">
-                <div className="footer_about">
-                    <div className="footer_about_info">
-                        <h1>About Us</h1>
-                        <div className='footer_about_info_p'>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium sapiente neque totam veritatis, assumenda a libero cupiditate suscipit inventore ab?</p>
+    return (
+        <footer className='footer'>
+            <div className="footer_container">
+                <div className="footer_container_top">
+                    <div className="footer_about">
+                        <div className="footer_about_info">
+                            <h1>About Us</h1>
+                            <div className='footer_about_info_p'>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium sapiente neque totam veritatis, assumenda a libero cupiditate suscipit inventore ab?</p>
+                            </div>
+                        </div>
+                        <div className="social_apps">
+                            <div className='social_apps_circle'>
+                                <SlSocialInstagram className="social_apps_icon" onClick={() => window.open("https://stackoverflow.com/questions/13071967/adding-an-onclick-function-to-go-to-url-in-javascript")} />
+                            </div>
+                            <div className='social_apps_circle'>
+                                <SlSocialFacebook className="social_apps_icon" onClick={() => window.open("https://stackoverflow.com/questions/13071967/adding-an-onclick-function-to-go-to-url-in-javascript")} />
+                            </div>
+                            <div className='social_apps_circle'>
+                                <SlSocialTwitter className="social_apps_icon" onClick={() => window.open("https://stackoverflow.com/questions/13071967/adding-an-onclick-function-to-go-to-url-in-javascript")} />
+                            </div>
                         </div>
                     </div>
-                    <div className="social_apps">
-                        <div className='social_apps_circle'>
-                            <SlSocialInstagram className="social_apps_icon" onClick={() => window.open("https://stackoverflow.com/questions/13071967/adding-an-onclick-function-to-go-to-url-in-javascript")} />
+                    <div className="footer_services">
+                        <h1>Services</h1>
+                        <div className="footer_services_links">
+                            <a href="#">Cooked</a>
+                            <a href="#">Deliver</a>
+                            <a href="#">Quality Foods</a>
+                            <a href="#">Mixed</a>
                         </div>
-                        <div className='social_apps_circle'>
-                            <SlSocialFacebook className="social_apps_icon" onClick={() => window.open("https://stackoverflow.com/questions/13071967/adding-an-onclick-function-to-go-to-url-in-javascript")} />
-                        </div>
-                        <div className='social_apps_circle'>
-                            <SlSocialTwitter className="social_apps_icon" onClick={() => window.open("https://stackoverflow.com/questions/13071967/adding-an-onclick-function-to-go-to-url-in-javascript")} />
+                    </div>
+                    <div className="footer_question">
+                        <h1>Have a questions?</h1>
+                        <div className="footer_questions_info">
+                            <div className="footer_question_info_box">
+                                <div className="footer_questions_icon">
+                                    <MdLocationPin className ='content_logo' size='20px' color='#fffa' />
+                                </div>
+                                <div className="footer_question_p">
+                                    <p>	203 Fake St. Mountain View, San Francisco, California, USA</p>
+                                </div>
+                            </div>
+                            <div className="footer_question_info_box">
+                                <div className="footer_questions_icon">
+                                    <BsFillTelephoneFill className ='content_logo' size='20px' color='#fffa' />
+                                </div>
+                                <div className="footer_question_p"><a href="tel:8987116415">8 987 116-94-15</a></div>
+                            </div>
+                            <div className="footer_question_info_box">
+                                <div className="footer_question_icon">
+                                    <MdEmail className ='content_logo' size='20px' color='#fffa' />
+                                </div>
+                                <div className="footer_question_p"><p>ranil2086@bk.ru</p></div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="footer_services">
-                    <h1>Services</h1>
-                    <div className="footer_services_links">
-                        <a href="#">Cooked</a>
-                        <a href="#">Deliver</a>
-                        <a href="#">Quality Foods</a>
-                        <a href="#">Mixed</a>
-                    </div>
-                </div>
-                <div className="footer_question">
-                    <h1>Have a questions?</h1>
-                    <div className="footer_questions_info">
-                        <div className="footer_question_info_box">
-                            <div className="footer_questions_icon">
-                                <MdLocationPin className ='content_logo' size='20px' color='#fffa' />
-                            </div>
-                            <div className="footer_question_p">
-                                <p>	203 Fake St. Mountain View, San Francisco, California, USA</p>
-                            </div>
-                        </div>
-                        <div className="footer_question_info_box">
-                            <div className="footer_questions_icon">
-                                <BsFillTelephoneFill className ='content_logo' size='20px' color='#fffa' />
-                            </div>
-                            <div className="footer_question_p"><a href="tel:8987116415">8 987 116-94-15</a></div>
-                        </div>
-                        <div className="footer_question_info_box">
-                            <div className="footer_question_icon">
-                                <MdEmail className ='content_logo' size='20px' color='#fffa' />
-                            </div>
-                            <div className="footer_question_p"><p>ranil2086@bk.ru</p></div>
-                        </div>
-                    </div>
+                <div className="footer_container_bottom">
+                    <p>Copyright ©2023 All rights reserved | This template is made with LOVE by AJAX</p>
                 </div>
             </div>
-            <div className="footer_container_bottom">
-                <p>Copyright ©2023 All rights reserved | This template is made with LOVE by AJAX</p>
-            </div>
-        </div>
-    </footer>
-  )
+        </footer>
+    )
 }
 
 export default Footer
